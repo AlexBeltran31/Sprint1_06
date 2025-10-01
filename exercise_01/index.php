ed1e82c92321945e2616ffbedce6829d0abe3dd3
AlexBeltran31 <alexbeltranrt@gmail.com>
2025-10-01 13:37:31 +0200

    Exercise 1 done

--- exercise_01/index.php
+++ exercise_01/index.php
@@ -1,1 +1,14 @@
 <?php
+session_start ();
+
+if (isset($_POST['name']) && isset($_POST['userId']) && isset($_POST['password'])) {
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['userId'] = $_POST['userId'];
+    $_SESSION['password'] = $_POST['password'];
+}
+
+if (isset($_SESSION['name'], $_SESSION['userId'], $_SESSION['password'])) {
+    echo "Name: " . $_SESSION['name'] . "\n";
+    echo "UserId: " . $_SESSION['userId'] . "\n";
+    echo "Password: " . $_SESSION['password'] . "\n";
+}
